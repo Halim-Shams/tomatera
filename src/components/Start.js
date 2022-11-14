0e1c3c7393a1a53c1bcc08cc74d0341447bc9ca8
Nicholas Ly <nichdly@gmail.com>
2022-11-14 11:10:00 -0500

    Forgot to add new files; fixed now

---
+++ src/components/Start.js
@@ -0,0 +1,9 @@
+import './Start.css'
+
+export default function Start() {
+    return (
+        <div className="component-start">
+            <button className='button'>Start</button>
+        </div>
+    )
+}
